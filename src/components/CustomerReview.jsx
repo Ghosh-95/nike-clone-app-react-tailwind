3f0a918f0ccd4dc272b2a758955f3d2296bba6b2
Sushovan <sushovanghosh960@gmail.com>
2024-06-14 21:21:46 +0530

    add customer review and styles

--- src/components/CustomerReview.jsx
+++ src/components/CustomerReview.jsx
@@ -1,7 +1,24 @@
+import { reviews } from "../utils/contants";
+import ReviewCard from "./ReviewCard";
+
 export default function CustomerReview() {
     return (
         <section className="bg-pale-blue padding">
-            customer reviews
+            <h2 className="hidden">Customer Reviews</h2>
+
+            <section id="customer-review" className="max-container">
+                <h3 className="text-center text-4xl font-bold font-palanquin">
+                    What our <span className="text-coral-red">People </span>
+                    love?
+                </h3>
+                <p className="info-text text-center m-auto max-w-lg mt-6">Hear what some of our beloved customers are saying about their experience and how much satisfied they are with us.</p>
+
+                <article className="mt-24 flex gap-14 flex-1 justify-center items-center max-xl:flex-col">
+                    {reviews.map(review => (
+                        <ReviewCard key={review.customerName} {...review} />
+                    ))}
+                </article>
+            </section>
         </section>
     )
 }
---
+++ src/components/ReviewCard.jsx
@@ -0,0 +1,21 @@
+export default function ReviewCard({ customerName, imgURL, rating, feedback }) {
+    return (
+        <div className="flex justify-center flex-col items-center gap-4">
+            <img src={imgURL} alt={customerName + " image"} className="rounded-full object-contain w-[5rem] h-[5rem]" />
+
+            <p className="font-montserrat text-slate-gray leading-5 text-center max-w-sm">{feedback}</p>
+
+            <p>
+                <span><i className="fa-solid fa-star text-coral-red"></i></span>
+                <span className="ml-2 text-slate-gray font-montserrat">({rating})</span>
+            </p>
+
+            <p className="text-xl font-palanquin ">{customerName}</p>
+
+            <span className="text-sm text-blue-400">
+                <i class="fa-solid fa-circle-check mr-1"></i>
+                Verified customer
+            </span>
+        </div>
+    )
+}

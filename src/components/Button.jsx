64b7cf4c330322466b039d59e4de15e2517e81e6
Sushovan <sushovanghosh960@gmail.com>
2024-06-15 10:01:56 +0530

    add subscribe section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,6 @@
-export default function Button({ label, iconURL, className }) {
+export default function Button({ label, iconURL, className, fullWidth }) {
     return (
-        <button className={`flex items-center justify-center gap-4 py-3 font-montserrat leading-none px-6 text-lg border-2 ${className ? className : 'bg-coral-red border-coral-red text-white'} rounded-full active:bg-red-500 active:border-red-500 active:text-white`}>
+        <button className={`flex ${fullWidth && 'w-full'} items-center justify-center gap-4 py-3 font-montserrat leading-none px-6 text-lg border-2 ${className ? className : 'bg-coral-red border-coral-red text-white'} rounded-full active:bg-red-500 active:border-red-500 active:text-white`}>
             {label}
             {iconURL && <img
                 src={iconURL}
--- src/components/Subscribe.jsx
+++ src/components/Subscribe.jsx
@@ -1,7 +1,25 @@
+import Button from "./Button";
+
 export default function Subscribe() {
     return (
         <section className="padding-x sm:py-32 py-16 w-full">
-            subscribe
+            <h2 className="hidden">Subscribe</h2>
+
+            <section className="max-container flex justify-between items-center max-lg:flex-col gap-10" id="contact-us">
+
+                <h3 className="text-4xl leading-[4.25rem] font-bold font-palanquin lg:max-w-md">
+                    Sign Up for
+                    <span className="text-coral-red"> Updates </span> & Newsletters
+                </h3>
+
+                <div className="lg:max-w-[40%] w-full flex items-center gap-5 p-2.5 max-sm:flex-col sm:border sm:border-slate-gray rounded-full">
+                    <input type="text" placeholder="nike@subscribe.com" className="input" />
+                    <div className="flex items-center max-sm:justify-end max-sm:w-full">
+                        <Button fullWidth="" label="Sign Up" />
+                    </div>
+                </div>
+
+            </section>
         </section>
     );
 };
